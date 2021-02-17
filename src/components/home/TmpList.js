87311bb236e461392c818e9024340aecb97f99ca
bepyan <bepyan@naver.com>
2021-02-18 01:23:10 +0900

    init

---
+++ src/components/home/TmpList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TmpList() {
+    return (
+        <div>
+            tmp list
+        </div>
+    )
+}
+
+export default TmpList
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import Header from 'components/Header';
+import TmpList from 'components/home/TmpList';
+import React from 'react'
+
+function Home () {
+    return (
+        <div>
+            <Header />
+            <TmpList />
+        </div>
+    );
+}
+
+export default Home;
