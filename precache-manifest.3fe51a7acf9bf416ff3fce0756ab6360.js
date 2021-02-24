9269b13243b9263c835621f7c117281e0d132a6a
bepyan <bepyan@naver.com>
2021-02-25 01:36:31 +0900

    Updates

---
+++ precache-manifest.3fe51a7acf9bf416ff3fce0756ab6360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc8394c9f76129e6628afc5dd9cab6bc",
+    "url": "/my-react/index.html"
+  },
+  {
+    "revision": "3cdfd58361dc584de6ee",
+    "url": "/my-react/static/js/2.491d6e45.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/my-react/static/js/2.491d6e45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d765b401a8fcac6bc52",
+    "url": "/my-react/static/js/main.954e99f2.chunk.js"
+  },
+  {
+    "revision": "ebe32448573dbd74b7a5",
+    "url": "/my-react/static/js/runtime-main.2b5d1e0a.js"
+  }
+]);
