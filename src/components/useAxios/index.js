ed81e82353775e3f2f1a69917c9da640e4606c9d
bepyan <rlantia97@kumoh.ac.kr>
2021-03-12 17:05:52 +0900

    [add] useAxios

--- src/components/useAxios/index.js
+++ src/components/useAxios/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import useAxios from 'hooks/useAxios'
 
 export default () => {
+    const { loading, err, data, refetch } = useAxios({ url: "http://aws.random.cat/meow" })
 
-
-    return <div>
-        useAxios
+    return <div style={{ textAlign: "center" }}>
+        <h4>Cat from AWS</h4>
+        <button onClick={refetch} >Get</button>
+        <br/>
+        <br/>
+        {loading ? <h4>{`🐈 Loading`}</h4> : <img src={data.file} alt="kit"/>}
+        <p>{err}</p>
     </div>
 }
