7c88aba01cb82bd7952c4cc8a3ee99dda5960a0c
bepyan <rlantia97@kumoh.ac.kr>
2021-03-22 17:52:55 +0900

    merge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from 'App';
@@ -11,15 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-=======
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from 'App';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
->>>>>>> 3f500653866facbfcf25fbcca56bd52916f9fa69
